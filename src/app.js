4513ed70053d5902948764ef50f49e3eb0a8db75
Gonzalo <gon.zalo.music97@gmail.com>
2021-03-18 12:03:15 -0600

    Project created, added types

---
+++ src/app.js
@@ -0,0 +1,5 @@
+var Client = require('fhir-kit-client');
+var fhirClient = new Client({
+    baseUrl: 'http://fhir.hl7fundamentals.org/r4'
+});
+console.log('I am just a skeleton, so I do nothing!');
---
+++ src/insertPractitioner.ts
@@ -0,0 +1,75 @@
+import Client from "fhir-kit-client";
+
+const fhirClient: Client = new Client({
+    baseUrl: 'http://fhir.hl7fundamentals.org/r4',
+    customHeaders: {
+        "Content-Type": "application/fhir+json",
+        "Accept": "application/fhir+json"
+    }
+
+});
+
+const newPractitioner: fhir.Practitioner = {
+
+    "resourceType": "Practitioner",
+    "identifier": [
+        {
+            "system": "(http://canada.gov/cpn",
+            "value": "51922"
+        }
+    ],
+    "active": true,
+    "name": [
+        {
+            "family": "Dellacroix",
+            "given": [
+                "Madeleine"
+            ]
+        }
+    ],
+    "telecom": [
+        {
+            "system": "phone",
+            "value": "613-555-0192 "
+        },
+        {
+            "system": "email",
+            "value": "qcpamxms9dq@groupbuff.com"
+        }
+    ],
+    "address": [
+        {
+            "line": [
+                "3766 Papineau Avenue"
+            ],
+            "city": "Montreal",
+            "state": "Quebec",
+            "postalCode": "H2K 4J5",
+            "country": "Canada"
+        }
+    ],
+    "qualification": [
+        {
+            "code": {
+                "coding": [
+                    {
+                        "system": "http://canada.gov/cpnq",
+                        "code": "OB/GYN"
+                    }
+                ]
+            }
+        }
+    ]
+};
+
+fhirClient.create({
+    resourceType: 'Practitioner',
+    body: newPractitioner,
+}).then((data) => {
+    var NewId = data.id;
+    console.log("Id:" + NewId);
+})
+    .catch((error) => {
+        var errorText = JSON.stringify(error);
+        console.log(errorText)
+    });
